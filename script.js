4e2330c0a8f7f1414987d7da384e8f66d4e5ff63
HolyyTheo <85359218+HolyyTheo@users.noreply.github.com>
2023-01-16 20:11:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
+//index sayfası dışında script dosyası ile istenilen fonksiyonu aldığım alan.
 let Etiket = document.querySelectorAll('.imgBx');
-let Don  = document.querySelectorAll('.imgBx1');
 window.onload = function () {
     Etiket.classList.remove('imgBx');
     Etiket.classList.add('imgBx active');
-    Don.classList.remove('imgBx1');
-    Don.classList.add('imgBx1 active');
 }
